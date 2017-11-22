3476abd566fd99f6f64684a214fa19b0eaec49e5
Alex <m_alex02@mail.ru>
2017-11-22 23:16:47 +0500

    Реализована ф-ция нахождения пар

--- 1.py
+++ 1.py
@@ -6,3 +6,28 @@ def isprime(n):
             return False
         else:
             return True
+		
+def generate_keypair(p, q):
+	cycle_var = False
+	
+	while cycle_var != True:
+		if not (isprime(p) and isprime(q)):
+			raise ValueError("'Both numbers must be prime.")
+		elif p == q:
+			raise ValueError('p and q cannot be equal')
+		else:
+			print("These numbers are prime")
+			cycle_var = True
+			
+			
+
+
+			
+	
+		
+		
+		
+		
+	
+			
+		
